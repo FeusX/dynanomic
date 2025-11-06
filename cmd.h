70f6895d3deacdf4563a485b4b0f62485c50eeb7
feusx <feusuaigax@proton.me>
2025-11-06 21:53:06 +0300

    first

---
+++ cmd.h
@@ -0,0 +1,33 @@
+#ifndef CMD_H
+#define CMD_H
+
+typedef void (*CmdFunc) (char *args);
+
+typedef struct {
+  const char *name;
+  CmdFunc func;
+} Command;
+
+const Command commands[] = {
+  { "WRITE LED", writeLed },
+  { "WRITE CMD", writeCmd},
+  { "HELP", help},
+  { "INPUT", getInput}
+};
+
+const int command_num = sizeof(commands) / sizeof(commands[0]);
+
+void writeLed(char **args, int argc) {
+  if(argc < 2) Serial.println("[ERROR] Usage: ledOn(pin, value)"); return;
+
+  int8_t pin = atoi(args[0]);
+  int8_t val = atoi(args[1]);
+
+  pinMode(pin, OUTPUT);
+
+  if(val > 255) Serial.println("[ERROR] value can't be greater than 255");
+
+  if(val > 1) analogWrite(pin, val);
+  else digitalWrite(pin, val ? HIGH : LOW);
+}
+#endif
